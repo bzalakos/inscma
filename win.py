d70afee2cd9ab4617e347e7b75ff90287264ca50
Benjamin Zalakos <bzalakos@gmail.com>
2016-12-15 17:21:32 +1100

    More stuff
    Changed back to a .py, for debugging purposes.
    Also, got the Shaders working, and now the VBO doesn't work with colour
    indices, so whatever.

--- win.py
+++ win.py
@@ -1,22 +1,22 @@
 """Draws a bunch of squares."""
 # Ignore the invalid variable naming. pylint: disable-msg=C0103
 import sys
+import time
 # from random import random
-from math import pi#, sin, cos, atan2
-from ctypes import c_float
+from math import pi#, sin#, cos, atan2
 from numpy import array
 
 import OpenGL.GL as GL
 from OpenGL.arrays import vbo
 from OpenGL.GL import shaders
-import OpenGL.GLU as GLU
+# import OpenGL.GLU as GLU
 import OpenGL.GLUT as GLUT
 
 window = 0
 colro = 0
 sides = 3
 pion3 = pi / 3
-
+# pylint: disable-msg=W0601
 # Y is indeed the up/down axis.
 walls = [[(-1, -1, -1), (1, -1, -1), (1, -1, 1), (-1, -1, 1),   # floor
           (-1, 1, -1), (1, 1, -1), (1, 1, 1), (-1, 1, 1)],      # plafond
@@ -30,8 +30,6 @@ room = [[6, 2, 2, 3],   # x +->
         [4, 0, 0, 1],   # z
         [4, 0, 0, 1],   # +
         [12, 8, 8, 9]]  # v
-
-vertex_size = 3 * c_float
 
 def tadd(*args):
     """Element-wise addition of tuples."""
@@ -49,18 +47,21 @@ def wall(x, y, side):
     res += [ttim(fill, w) for i in range(4) for w in walls[i+1] if side & 2**i]
     return [tadd((x, 0, y), tadd(ttim(scale, r), offset)) for r in res]
 
-vs = [t for y, i in enumerate(room) for x, w in enumerate(i) for t in wall(x, y, w)
-     ] + [(0.1, 0.1, 0), (-0.1, -0.1, 0), (0.1, -0.1, 0)]
+# vs = [t for y, i in enumerate(room) for x, w in enumerate(i) for t in wall(x, y, w)]
+vs = [(0.1, 0.1, 0, 1, 1, 1), (0.1, -0.1, 0, 1, 1, 1),
+      (-0.1, -0.1, 0, 1, 1, 1), (-0.1, 0.1, 0, 1, 1, 1)]
 
 def rematr(wid, hig):
     """Resets the projection matrix."""
-    GL.glMatrixMode(GL.GL_PROJECTION)
-    GL.glLoadIdentity()
-    GLU.gluPerspective(45.0, wid / hig, 0.1, 100.0)
-    GL.glMatrixMode(GL.GL_MODELVIEW)
+    wid, hig = hig, wid
+    # GL.glMatrixMode(GL.GL_PROJECTION)
+    # GL.glLoadIdentity()
+    # GLU.gluPerspective(45.0, wid / hig, 0.1, 100.0)
+    # GL.glMatrixMode(GL.GL_MODELVIEW)
 
 def init(wid, hig):
     """Initialises the display."""
+    global shaderp
     GL.glClearColor(0.0, 0.2, 0.15, 0.0)
     GL.glClearDepth(1.0)
     GL.glDepthFunc(GL.GL_LESS)
@@ -69,18 +70,25 @@ def init(wid, hig):
     GL.glEnable(GL.GL_TEXTURE_2D)
     rematr(wid, hig)
 
-    # # Set up the Shaders as well.
-    # vertex_shader = shaders.compileShader("""#version 330 core
-    # layout (location = 0) in vec3 position;
-    # void main(){
-    # gl_Position = vec4(position.x, position.y, position.z, 1.0);
-    # }""", GL.GL_VERTEX_SHADER)
-    # fragment_shader = shaders.compileShader("""#version 330 core
-    # out vec4 colour;
-    # void main(){
-    # colour = vec4(1.0, 0.5, 0.2, 1.0);
-    # }""", GL.GL_FRAGMENT_SHADER)
-    # GL.glUseProgram(shaders.compileProgram(vertex_shader, fragment_shader))
+    # Set up the Shaders as well.
+    vertex_shader = shaders.compileShader("""#version 330 core
+    layout (location = 0) in vec3 position;
+    layout (location = 1) in vec3 colour;
+    uniform vec4 projectmatrix;
+    out vec3 ourcolour;
+    void main(){
+    gl_Position = vec4(position.xyz, 1.0);
+    ourcolour = colour;
+    }""", GL.GL_VERTEX_SHADER)
+    #uniform vec4 ourcolour;
+    fragment_shader = shaders.compileShader("""#version 330 core
+    in vec3 ourcolour;
+    out vec4 colour;
+    void main(){
+    colour = vec4(ourcolour.xyz, 1.0);
+    }""", GL.GL_FRAGMENT_SHADER)
+    shaderp = shaders.compileProgram(vertex_shader, fragment_shader)
+    GL.glUseProgram(shaderp)
 
 def resiz(wid, hig):
     """Handles matrix resizing on viewport resize."""
@@ -94,35 +102,29 @@ def buff_vertices(verts: list) -> vbo.VBO:
     v = vbo.VBO(array(verts, 'f'))
     v.bind()
     GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
-    GL.glVertexPointerf(v)  # ughghhggghhhhgggg   pylint: disable-msg=E1101
+    GL.glEnableClientState(GL.GL_COLOR_ARRAY)
+    GL.glVertexPointer(3, GL.GL_FLOAT, 24, v)
+    GL.glColorPointer(3, GL.GL_FLOAT, 24, v+12)
     GL.glDrawArrays(GL.GL_QUADS, 0, len(verts))
     v.unbind()
+    GL.glDisableClientState(GL.GL_COLOR_ARRAY)
     GL.glDisableClientState(GL.GL_VERTEX_ARRAY)
 
 def draw():
     """Put the main drawing code in here."""
+    global lasttime
+    timedelta = time.time() - lasttime
     GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
     GL.glLoadIdentity()
 
-    GL.glMatrixMode(GL.GL_PROJECTION)
-    GL.glRotatef(0.01, 0, 1, 0)
-    GL.glMatrixMode(GL.GL_MODELVIEW)
     buff_vertices(vs)
-    #
-    # GL.glBegin(GL.GL_QUADS)
-    # GL.glColor3f(1, 0, 0)
-    #
-    # for v in vs:
-    #     GL.glVertex3f(*v)
-    #
-    # GL.glEnd()
 
     GLUT.glutSwapBuffers()
+    lasttime = time.time()
 
 def main():
     """Do all this upon running the script."""
-    global window
-
+    global window, firsttime, lasttime
     GLUT.glutInit(sys.argv)
     GLUT.glutInitDisplayMode(GLUT.GLUT_RGBA | GLUT.GLUT_DOUBLE | GLUT.GLUT_DEPTH)
     GLUT.glutInitWindowSize(640, 480)
@@ -131,6 +133,7 @@ def main():
     GLUT.glutIdleFunc(draw)
     GLUT.glutReshapeFunc(resiz)
     init(640, 480)
+    firsttime = lasttime = time.time()
     GLUT.glutMainLoop()
 
 if __name__ == '__main__':
