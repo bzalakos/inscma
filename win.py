525b910aa841145f46a80240b447bfa30235052e
Benjamin Zalakos <bzalakos@gmail.com>
2017-01-03 17:42:07 +1100

    Many Change
    Quite a lot of fixes here.
    
    Changed from transformations to pyrr, if only so as to not have to call
    .transpose() every single time.
    
    Added a view matrix for translations, fixed the view matrix from wierd
    hyperbolic mode, fixed the modelview matrix to only have rotations,
    removed manual perspective.
    
    Removed all terrain sizing from the vertex assembly, which had a side
    effect of simplifying it a good bit.
    Put the fill value in a constant just before the initial array
    declaration, put the scaling factor into the modelview matrix.
    
    Put a whole bunch of padding into the vertex declarations, noticed the
    colours were all wrong, should be #FFFFFF, not #000000. Also, the
    y-plates need four values each, not three.
    
    Added a VAO thing. A way to group vbos together?
    
    WASD and MMOV to change the view and modelview matrices respectively.
    
    Also, fixed all that wierd whatever it was that was going so badly
    before. It actually works now, so I must have done something right,
    right?

--- win.py
+++ win.py
@@ -8,11 +8,11 @@ from math import pi, tan#sin#, cos, atan2
 from PIL import Image
 from numpy import array
 
+from pyrr import Matrix44 as matrix
 import OpenGL.GL as GL
 from OpenGL.arrays import vbo
 from OpenGL.GL import shaders
 import OpenGL.GLUT as GLUT
-# import transformations as TR
 
 os.chdir(os.path.dirname(__file__))
 
@@ -22,26 +22,28 @@ sides = 3
 pion3 = pi / 3
 # pylint: disable-msg=W0601
 # Y is indeed the up/down axis. x y z r g b u v
-vers = [[[-1, -1, -1], [1, -1, -1], [1, -1, 1], [-1, -1, 1],   # sol
-         [-1, 1, -1], [1, 1, -1], [1, 1, 1], [-1, 1, 1]],      # plafond
-        [[1, 1, -1], [1, 1, 1], [1, -1, 1], [1, -1, -1]],      # +x
-        [[-1, 1, -1], [1, 1, -1], [1, -1, -1], [-1, -1, -1]],  # -z
-        [[-1, 1, -1], [-1, 1, 1], [-1, -1, 1], [-1, -1, -1]],  # -x
-        [[-1, 1, 1], [1, 1, 1], [1, -1, 1], [-1, -1, 1]],      # +z
+l = 0.49   # To ease the sizing, use this constant.
+vers = [[[-l, -l, -l], [+l, -l, -l], [+l, -l, +l], [-l, -l, +l],   # sol
+         [-l, +l, -l], [+l, +l, -l], [+l, +l, +l], [-l, +l, +l]],  # plafond
+        [[+l, +l, -l], [+l, +l, +l], [+l, -l, +l], [+l, -l, -l]],  # +x
+        [[-l, +l, -l], [+l, +l, -l], [+l, -l, -l], [-l, -l, -l]],  # -z
+        [[-l, +l, -l], [-l, +l, +l], [-l, -l, +l], [-l, -l, -l]],  # -x
+        [[-l, +l, +l], [+l, +l, +l], [+l, -l, +l], [-l, -l, +l]],  # +z
        ]
 
 # Not so bad when there's a bunch of repetition
-uval = [[[0.75, 1], [0.875, 1], [0.875, 0.75], [0.75, 0.75], [0.875, 1], [0.9375, 1],
-         [0.9375, 0.875], [0.875, 0.875]]] + [[[0.5, 1], [0.75, 1], [0.75, 0.5], [0.5, 0.5]]] * 4
+uval = [[[0.7500, 0.000], [0.8750, 0.000], [0.8750, 0.250], [0.750, 0.250],
+         [0.8750, 0.000], [0.9375, 0.000], [0.9375, 0.125], [0.875, 0.125]]] \
+     + [[[0.5000, 0.000], [0.7500, 0.000], [0.7500, 0.500], [0.500, 0.500]]] * 4
 
 # Sure is easy when they're all zeroes.
-blankol = [[[0] * 3] * 6] + [[[0] * 3] * 4] * 4
+blankol = [[[1] * 3] * 8] + [[[1] * 3] * 4] * 4
 
 walls = [[sum(x, []) for x in zip(vers[y], blankol[y], uval[y])] for y in range(len(vers))]
 
 room = [[6, 2, 2, 3],   # x +->
-        [4, 0, 0, 1],   # z
-        [4, 0, 0, 1],   # +
+        [4, 1, 0, 1],   # z
+        [4, 8, 2, 1],   # +
         [12, 8, 8, 9]]  # v
 
 def tadd(*args):
@@ -54,43 +56,29 @@ def ttim(scal, ite):
 
 def wall(x, y, side):
     """Returns a list of lists of floats that forms a space."""
-    scale, fill = 4.8, 0.9/2
-    offset = (0, 0, -5)
-    res = [ttim(fill, w[:3]) + w[3:] for w in walls[0]]
+    res = [w for w in walls[0]]
     # Skip zero here, just did it.
-    res += [ttim(fill, w[:3]) + w[3:] for i in range(4) for w in walls[i+1] if side & 2**i]
+    res += [w for i in range(4) for w in walls[i+1] if side & 2**i]
     # tile coordinates offset, tile size multiplier
-    return [tadd((x, 0, y), tadd(ttim(scale, r[:3]), offset)) + r[3:] for r in res]
+    return [tadd([x, 0, y], r[:3]) + r[3:] for r in res]
 
-# vs = [t for y, i in enumerate(room) for x, w in enumerate(i) for t in wall(x, y, w)]
-vs = [(+1.0, +1.0, +0.0,
-       +1.0, +1.0, +0.0,
-       +0.0, +0.0),
-      (+1.0, -1.0, +0.0,
-       +1.0, +0.0, +0.0,
-       +0.0, +1.0),
-      (-1.0, -1.0, -0.0,
-       +0.0, +0.0, +1.0,
-       +1.0, +1.0),
-      (-1.0, +1.0, -0.0,
-       +0.0, +1.0, +0.0,
-       +1.0, +0.0)]
+vs = [t for y, i in enumerate(room) for x, w in enumerate(i) for t in wall(x, y, w)]
 
 def rematr(wid, hig):
     """Resets the projection matrix."""
+    global viewmatr
     if hig == 0:
         hig = 1
     v, a, n, f = 1/tan(pi/8), wid/hig, 0.01, 100
-    print(v/a, v, (f+n)/(f-n), 2*f*n/(n-f))
-    # pm = array([[v/a, 0, 0, 0], [0, v, 0, 0], [0, 0, (f+n)/(n-f), -1], [0, 0, 2*f*n/(n-f), 0]], 'f')
-    pm = array([[v/a, 0, 0, 0], [0, v, 0, 0], [0, 0, (f+n)/(n-f), -1], [0, 0, 2*f*n/(n-f), 0]], 'f')
-    mm = array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, -5, 1]], 'f')
-    # mm = array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0.5,0,-5,1]], 'f')
+    # pyrr has FOV in degrees for some reason???
+    pm = matrix.perspective_projection(75, wid/hig, 0.01, 100)
+    viewmatr = matrix.from_translation([0, 0, -5])
     GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'projectmatrix'), 1, GL.GL_FALSE, pm)
-    GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'modelmatrix'), 1, GL.GL_FALSE, mm)
+    GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'viewmatrix'), 1, GL.GL_FALSE, viewmatr)
 
 def init(wid, hig):
     """Initialises the display."""
+    global modelmtx
     GL.glClearColor(0.0, 0.2, 0.15, 0.0)
     GL.glClearDepth(1.0)
     GL.glDepthFunc(GL.GL_LESS)
@@ -99,6 +87,8 @@ def init(wid, hig):
     GL.glEnable(GL.GL_TEXTURE_2D)
     shades()
     rematr(wid, hig)
+    modelmtx = matrix.from_scale([1.8, 1.8, 1.8])
+    GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'modelmatrix'), 1, GL.GL_FALSE, modelmtx)
 
 def texturit(filn: str) -> int:
     """Import a Texture."""
@@ -121,26 +111,22 @@ def shades():
     layout (location = 1) in vec3 colour;
     layout (location = 2) in vec2 texcord;
     uniform mat4 projectmatrix;
+    uniform mat4 viewmatrix;
     uniform mat4 modelmatrix;
     out vec3 ourcolour;
     out vec2 ourtex;
     void main(){
-        gl_Position = projectmatrix * modelmatrix * vec4(position, 1.0);
+        gl_Position = projectmatrix * viewmatrix * modelmatrix * vec4(position, 1.0);
         ourcolour = colour;
         ourtex = texcord;
     }""", GL.GL_VERTEX_SHADER)
-    # if(colour == vec3(0,0,0))
-    #   ourcolour = vec3(1,0,0);
-    # else
-    # ourcolour = vec3(0,1,0);
-    #uniform vec4 ourcolour;
     fragment_shader = shaders.compileShader("""#version 330 core
     in vec3 ourcolour;
     in vec2 ourtex;
     out vec4 colour;
     uniform sampler2D tex;
     void main(){
-        colour = texture(tex, ourtex);
+        colour = texture(tex, ourtex) * vec4(ourcolour, 1.0);
     }""", GL.GL_FRAGMENT_SHADER)
     shaderp = shaders.compileProgram(vertex_shader, fragment_shader)
     GL.glUseProgram(shaderp)
@@ -152,8 +138,11 @@ def resiz(wid, hig):
     GL.glViewport(0, 0, wid, hig)
     rematr(wid, hig)
 
-def buff_vertices(verts: list) -> vbo.VBO:
-    """Given a list of vertex-like objects, sets up a VBO and draws it. Then unsets the vbo."""
+def buff_vertices(verts: list, indes: list) -> int:
+    """Given a list of vertex-like objects, and maybe a list of indices sets up a thing. VAO?"""
+    vao = GL.glGenVertexArrays(1)
+    GL.glBindVertexArray(vao)
+
     v = vbo.VBO(array(verts, 'f'))
     v.bind()
     GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, False, 32, v)
@@ -162,28 +151,56 @@ def buff_vertices(verts: list) -> vbo.VBO:
     GL.glEnableVertexAttribArray(0)
     GL.glEnableVertexAttribArray(1)
     GL.glEnableVertexAttribArray(2)
-    GL.glDrawArrays(GL.GL_QUADS, 0, len(verts))
-    v.unbind()
-    GL.glDisableVertexAttribArray(0)
-    GL.glDisableVertexAttribArray(1)
-    GL.glDisableVertexAttribArray(2)
+    # Maybe you want to include the vertices verbatim? Go for it.
+    if indes is not None:
+        pass
+
+    GL.glBindVertexArray(0)
+    return vao
 
 def draw():
     """Put the main drawing code in here."""
-    global lasttime
+    global lasttime#, modelmtx
     timedelta = time.time() - lasttime
     GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
+    # modelmtx = matrix.from_y_rotation(0.001/timedelta+1)
+    GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'modelmatrix'), 1, GL.GL_FALSE, modelmtx)
 
     GL.glBindTexture(GL.GL_TEXTURE_2D, terrain)
-    buff_vertices(vs)
+    GL.glBindVertexArray(architincture)
+    GL.glDrawArrays(GL.GL_QUADS, 0, len(vs))
+    GL.glBindVertexArray(0)
     GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
 
     GLUT.glutSwapBuffers()
     lasttime = time.time()
 
+def onkey(code, x, y):
+    """Alter Vue Mater"""
+    global viewmatr
+    d = {b'w': [0, 1, 0], b's': [0, -1, 0],
+         b'd': [1, 0, 0], b'a': [-1, 0, 0],
+         b'e': [0, 0, 1], b'q': [0, 0, -1],}
+    if code == b'\x1b': # The Escape Key codevalue.
+        exit()
+    viewmatr = viewmatr * matrix.from_translation(d.get(code, [0, 0, 0]))
+    GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'viewmatrix'), 1, GL.GL_FALSE, viewmatr)
+
+def onmou(x, y):
+    """Alter Moda Mater"""
+    global modelmtx, moux, mouy
+    modelmtx = modelmtx * matrix.from_y_rotation((moux-x)/99) * matrix.from_x_rotation((mouy-y)/99)
+    GL.glUniformMatrix4fv(GL.glGetUniformLocation(shaderp, 'modelmatrix'), 1, GL.GL_FALSE, modelmtx)
+    moux, mouy = x, y
+
+def oncl(code, state, x, y):
+    """Inite Curso Posit"""
+    global moux, mouy
+    moux, mouy = x, y
+
 def main():
     """Do all this upon running the script."""
-    global window, firsttime, lasttime, terrain
+    global window, firsttime, lasttime, terrain, architincture
     GLUT.glutInit(sys.argv)
     GLUT.glutInitDisplayMode(GLUT.GLUT_RGBA | GLUT.GLUT_DOUBLE | GLUT.GLUT_DEPTH)
     GLUT.glutInitWindowSize(640, 480)
@@ -191,9 +208,12 @@ def main():
     GLUT.glutDisplayFunc(draw)
     GLUT.glutIdleFunc(draw)
     GLUT.glutReshapeFunc(resiz)
-    GLUT.glutKeyboardFunc(lambda *args: exit())
+    GLUT.glutKeyboardFunc(onkey)
+    GLUT.glutMotionFunc(onmou)
+    GLUT.glutMouseFunc(oncl)
     init(640, 480)
     terrain = texturit('terrain.png')
+    architincture = buff_vertices(vs, None)
     firsttime = lasttime = time.time()
     GLUT.glutMainLoop()
 
